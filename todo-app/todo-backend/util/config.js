788495342067b3731168a4974bacf44a14957e95
Dmitri9149 <dmitri.as.name@gmail.com>
2024-03-29 16:57:38 +0200

    add REDIS_URL to config, it seemd the ex 12_9 is solved

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -4,5 +4,6 @@ const REDIS_URL = process.env.REDIS_URL || undefined
 module.exports = {
   MONGO_URL: 'mongodb://the_username:the_password@localhost:3456/the_database',
   //: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL//: '//localhost:6378'
+  REDIS_URL: 'redis://localhost:6379'
+  //: '//localhost:6378'
 }
