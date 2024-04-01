c10460c1195127c5df711dd24a0dc72b3fe0a865
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-01 15:24:52 +0300

    make simple tests just to check setups

---
+++ todo-app/todo-frontend/src/tests/Todo.test.jsx
@@ -0,0 +1,7 @@
+import { render, fireEvent } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import Todo from '../Todos/Todo.jsx'
+
+test("check setups", () => {
+  expect(3).toBe(3);
+})
