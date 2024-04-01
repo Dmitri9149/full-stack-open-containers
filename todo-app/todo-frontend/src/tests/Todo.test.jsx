170d81d22502ccf517fdac698b0d6b2c89095a25
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-01 16:20:55 +0300

    make simplest possible test for simple ( initial) Todo component

--- todo-app/todo-frontend/src/tests/Todo.test.jsx
+++ todo-app/todo-frontend/src/tests/Todo.test.jsx
@@ -1,6 +1,19 @@
 import { render, fireEvent } from '@testing-library/react'
 import { screen } from '@testing-library/react'
+import Todo from '../Todos/Todo.jsx'
 
 test("check setups", () => {
   expect(3).toBe(3);
+})
+
+test("simplest test for rendering todo correctly", () => {
+  const todo = {
+    text: "Test todo",
+    done: false,
+  }
+  const deleteTodo = vi.fn()
+  const completeTodo = vi.fn()
+
+  render(<Todo todo={todo} deleteTodo={deleteTodo} completeTodo={completeTodo} />)
+  expect(screen.getByText("Test todo")).toBeInTheDocument()
 })
