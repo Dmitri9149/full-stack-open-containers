eb735861cddb41ad8f3bccab4c5d12c5e08d23fc
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-01 18:31:21 +0300

    define the Todo component

--- todo-app/todo-frontend/src/Todos/Todo.jsx
+++ todo-app/todo-frontend/src/Todos/Todo.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
 const Todo = ({ todo, deleteTodo, completeTodo }) => {
-  return (
+
+  const done = (
     <div>
+      <span>Is done</span>
+      <span>
+        <button onClick = {deleteTodo()}>Delete</button> 
+      </span>
+    </div>
+  )
+
+  const notDone = (
+    <div>
+      <span>Not done yet</span>
+        <button onClick = {completeTodo()}>Set as done</button>
+        <button onClick = {deleteTodo()}>Delete</button>
+    </div>
+  )
+
+  return (
+    <div
+      style = {{
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        maxWidth: '70%',
+        margin: 'auto'
+      }}
+    >
       <span>{todo.text}</span>
-      <span>{todo.done}</span>
+      <span>{todo.done ? done : notDone}</span>
     </div>
   )
 }
