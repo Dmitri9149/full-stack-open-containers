57c2d53e46b80c342c38539accc6b25469107a63
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-01 15:25:47 +0300

    remove import of unexisted file

--- todo-app/todo-frontend/src/tests/Todo.test.jsx
+++ todo-app/todo-frontend/src/tests/Todo.test.jsx
@@ -1,6 +1,5 @@
 import { render, fireEvent } from '@testing-library/react'
 import { screen } from '@testing-library/react'
-import Todo from '../Todos/Todo.jsx'
 
 test("check setups", () => {
   expect(3).toBe(3);
