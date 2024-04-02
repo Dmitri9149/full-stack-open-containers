89046b84be7b5662a99dbbc55b7dc6486185ef89
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-02 16:06:07 +0300

    comments

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,  // defined in .env file 
+  baseURL: import.meta.env.VITE_BACKEND_URL,  
 })
 
 export default apiClient
