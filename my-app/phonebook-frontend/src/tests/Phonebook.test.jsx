c5a9e86de241d81dced89711c9b875e40141e9bd
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-14 19:21:59 +0300

    add simple test for rendering Person record

--- my-app/phonebook-frontend/src/tests/Phonebook.test.jsx
+++ my-app/phonebook-frontend/src/tests/Phonebook.test.jsx
@@ -6,40 +6,23 @@ test("check setups", () => {
   expect(3).toBe(3);
 })
 
-/*
-test("simple test for rendering todo correctly", () => {
-  const todo = {
-    text: "Testing todo",
-    done: false,
+
+test("simple test for rendering phone record correctly", () => {
+  const record = {
+    name: "Test name",
+    number: "123456"
   }
-  const deleteTodo = vi.fn()
-  const completeTodo = vi.fn()
+  const remove = vi.fn()
 
-  render(<Todo todo={todo} deleteTodo={deleteTodo} completeTodo={completeTodo} />)
 
-  expect(screen.getByText("Testing todo")).toBeInTheDocument()
-  expect(screen.getByText("This todo is not done")).toBeInTheDocument()
-  expect(screen.getByText("Testing todo")).toBeDefined()
-  expect(screen.queryByText("This to do is done")).toBeNull()
+  render(<Person name={record.name} number={record.number} remove={remove} />)
 
-  fireEvent.click(screen.getByText("Set as done"))
-  expect(completeTodo).toHaveBeenCalledTimes(1)
-  expect(completeTodo).toHaveBeenCalledWith(todo)
+  expect(screen.getByText("Test name")).toBeInTheDocument()
+  expect(screen.getByText("123456")).toBeInTheDocument()
+  expect(screen.getByText("Test name")).toBeDefined()
+  expect(screen.queryByText("123456")).toBeDefined()
 
-  fireEvent.click(screen.getByText("Delete"))
-  expect(deleteTodo).toHaveBeenCalledTimes(1)
-  expect(deleteTodo).toHaveBeenCalledWith(todo)
-
-// assume that the buttons are ordered by design as 'Delete' 'Set as done'
-  const [firstButton, secondButton] = screen.getAllByRole('button')
-  fireEvent.click(firstButton)
-  expect(deleteTodo).toHaveBeenCalledWith(todo)
-  fireEvent.click(secondButton)
-  expect(completeTodo).toHaveBeenCalledWith(todo)
-
-  expect(deleteTodo).toHaveBeenCalledTimes(2)
-  expect(completeTodo).toHaveBeenCalledTimes(2)
+  fireEvent.click(screen.getByText("delete"))
+  expect(remove).toHaveBeenCalledTimes(1)
 
 })
-
-*/
